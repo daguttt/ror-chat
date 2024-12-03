bd317c7033f0dfca25c0dee156f170b6cf4b4763
daguttt <dagutmu667@gmail.com>
2024-12-03 11:08:21 -0500

    feat: validate that prompt `name` attr is not empty

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,7 +1,7 @@
 class Prompt < ApplicationRecord
   has_one :response
 
-  validates :name, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { minimum: 5 }
 
   attribute :processed_in_job, :boolean, default: false
