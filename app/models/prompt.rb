b595e2f6f4412b64b581ee98ea078a73c5c7d6a9
daguttt <dagutmu667@gmail.com>
2024-12-03 11:03:22 -0500

    feat: add basic validations and default values to models

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,3 +1,8 @@
 class Prompt < ApplicationRecord
   has_one :response
+
+  validates :name, length: { maximum: 255 }
+  validates :content, presence: true, length: { minimum: 5 }
+
+  attribute :processed_in_job, :boolean, default: false
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,6 @@
 class Response < ApplicationRecord
   belongs_to :prompt
   enum :status, [ :pending, :completed, :failed ]
+
+  attribute :status, default: :pending
 end
