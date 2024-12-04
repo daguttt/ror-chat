1ccdb405a27d77da926a0c74656f60d54463696b
daguttt <dagutmu667@gmail.com>
2024-12-04 09:35:58 -0500

    feat: add default name prompt attribute

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -4,5 +4,6 @@ class Prompt < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { minimum: 5, maximum: 10000 }
 
+  attribute :name, :string, default: "Untitled"
   attribute :processed_in_job, :boolean, default: false
 end
