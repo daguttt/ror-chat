7bcd4eae2e799808ddb2e4ff93275b94682aa4b5
daguttt <dagutmu667@gmail.com>
2024-12-04 09:35:33 -0500

    feat: add messages for length validation

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -2,7 +2,7 @@ class Prompt < ApplicationRecord
   has_one :response
 
   validates :name, presence: true, length: { maximum: 255 }
-  validates :content, presence: true, length: { minimum: 5 }
+  validates :content, presence: true, length: { minimum: 5, maximum: 10000 }
 
   attribute :processed_in_job, :boolean, default: false
 end
