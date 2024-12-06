de44fd6724d5c8baed7e8fbf66a116dfabb41efb
daguttt <dagutmu667@gmail.com>
2024-12-05 23:30:57 -0500

    feat: add a counter for untitled prompt's name attr

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,9 +1,18 @@
 class Prompt < ApplicationRecord
   has_one :response
 
+  before_save :modify_default_name
+
   validates :name, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { minimum: 5, maximum: 10000 }
 
   attribute :name, :string, default: "Untitled"
   attribute :processed_in_job, :boolean, default: false
+
+  def modify_default_name
+    return unless self.name == "Untitled"
+
+    prompts_untitled = self.class.where(name: "Untitled").count
+    self.name = "Untitled #{prompts_untitled}" if self.name == "Untitled"
+  end
 end
